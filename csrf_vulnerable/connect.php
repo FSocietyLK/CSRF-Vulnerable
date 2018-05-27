681fe86ea165bc054d5755cc87e559372ecb0674
FSocietyLK <yasey.omal@my.sliit.lk>
2018-05-27 13:16:05 +0530

    Update connect.php

--- csrf_vulnerable/connect.php
+++ csrf_vulnerable/connect.php
@@ -2,7 +2,7 @@
 	define('mysql_host','localhost');
 	define('mysql_user','root');
 	define('mysql_pass','');
-	define('mysql_db','db');
+	define('mysql_db','user');
 	
 	mysql_connect(mysql_host,mysql_user,mysql_pass,mysql_db);
 ?>
